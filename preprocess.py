00642773754595f0bb3fb7507db1e7d8bdeb8370
Juha Kiili <juha@valohai.com>
2021-01-26 13:09:05 +0200

    fix yaml

--- preprocess.py
+++ preprocess.py
@@ -11,7 +11,7 @@ data = {
 
 valohai.prepare(step="preprocess", parameters={}, inputs=data)
 
-df_train = pd.read_csv(get_input_path("training"))
+df_train = pd.read_csv(get_input_path("data/*.csv"))
 df_dictionary = pd.read_csv(get_input_path("dictionary"))
 df_preprocessed = preprocess(df_train, df_dictionary)
 df_preprocessed.to_csv(get_output_path("data/data.csv"), index=False)
